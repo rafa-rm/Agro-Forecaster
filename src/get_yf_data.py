32910a2ad9951d84ff5f6e2f6813510963244691
rafa-rm <rafaelrochamaciel@hotmail.com>
2026-01-30 15:55:59 -0300

    feat: migrate from CEPEA to Yahoo Finance for historical data fetching; remove obsolete code and update Terraform configurations

---
+++ src/get_yf_data.py
@@ -0,0 +1,97 @@
+import json
+import os
+import boto3
+import requests
+import pandas as pd
+from io import BytesIO, StringIO
+from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
+import yfinance as yf
+from botocore.config import Config
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+MAX_WORKERS = 50 
+
+my_config = Config(
+    max_pool_connections=50,
+    retries = {'max_attempts': 3}
+)
+s3 = boto3.client('s3', config=my_config)
+bucket_name = os.environ['S3_BUCKET_NAME']
+
+
+def upload_single_row(name: str, row):
+    
+    single_data = pd.DataFrame([row])
+    date_row = row['Date']
+    S3_PATH = f"raw/{name}/" + f"year={date_row.year}/month={date_row.month}/day={date_row.day}/data.parquet"
+    out_buffer = BytesIO()
+    single_data.to_parquet(out_buffer, index=False, engine='fastparquet', compression='snappy')
+
+    try:
+        s3.put_object(Bucket=bucket_name, Key=S3_PATH, Body=out_buffer.getvalue())
+        return f"✅ Uploaded {S3_PATH}"
+    except Exception as e:
+        return f"❌ Error {S3_PATH}: {str(e)}"
+
+def process_historical_yahoo_data(name: str, symbol: str) -> bool:
+    """
+    Fetches historical data from Yahoo Finance based on the provided name and ID.
+    Args:
+        name (str): The name.
+        symbol (str): The Yahoo Finance symbol.
+    Returns:
+        bool: True if successful, False otherwise.
+    """
+    try:
+        current_date = datetime.now().strftime('%Y-%m-%d')
+        ticker = yf.Ticker(symbol)
+        df = ticker.history(start="2010-01-01", interval="1d")
+
+        if df.empty:
+            print(f"⚠️ No data for {name}")
+            return False
+        
+        raw_data = df.copy()[['Date', 'Open', 'High', 'Low', 'Close']]
+        raw_data.reset_index(inplace=True)
+        raw_data['Date'] = raw_data['Date'].dt.strftime('%Y-%m-%d')
+        
+    except Exception as e:
+        print(f"Error processing data for {name}: {e}")
+        return False
+
+    rows_to_process = [row for _, row in raw_data.iterrows()]
+    
+    print(f"Starting parallel upload of {len(rows_to_process)} records for {name}...")
+    try:
+        with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+            results = list(executor.map(lambda row: upload_single_row(name, row), rows_to_process))
+
+        print(f"Finished. Processed {len(results)} items.")
+        return True
+    except Exception as e:
+        print(f"Error during parallel upload for {name}: {e}")
+        return False
+    
+
+
+def lambda_handler(event, context):
+
+    tickers = [
+        {"name": "soybean", "symbol": "ZS=F"},  # Soy Futures
+        {"name": "corn", "symbol": "ZC=F"},     # Corn Futures
+        {"name": "usd_brl", "symbol": "BRL=X"}  # Dollar to Real Exchange Rate
+    ]
+    results = {}
+    
+    if event.get('mode') == 'historical':
+        for ticker in tickers:
+            success = process_historical_yahoo_data(ticker["name"], ticker["symbol"])
+            results[ticker["name"]] = "Success" if success else "Failed"
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(results)
+    }
