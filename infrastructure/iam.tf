12b723cb5c197879783037792cebf043dd1a088e
rafa-rm <rafaelrochamaciel@hotmail.com>
2026-02-11 15:33:59 -0300

    feat(infra): refactor IAM roles for least privilege and pipeline chaining

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -1,132 +1,132 @@
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "agro-lambda-role"
+data "aws_iam_policy_document" "lambda_trust" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
 
+resource "aws_iam_role" "raw_role" {
+  name               = "agro-raw-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+}
+
+resource "aws_iam_role" "trusted_role" {
+  name               = "agro-trusted-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+}
+
+resource "aws_iam_role" "enriched_role" {
+  name               = "agro-enriched-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+}
+
+
+resource "aws_iam_role_policy" "raw_permissions" {
+  name = "raw-permissions"
+  role = aws_iam_role.raw_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow", 
+        Action = ["s3:PutObject", "s3:ListBucket", "s3:GetObject"],
+        Resource = [
+          aws_s3_bucket.agro_data_lake.arn,       
+          "${aws_s3_bucket.agro_data_lake.arn}/*"
+        ] 
+      },
+      {
+        Effect = "Allow", Action = "lambda:InvokeFunction",
+        Resource = aws_lambda_function.agro_processor.arn 
+      },
+      {
+        Effect = "Allow", 
+        Action = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "trusted_permissions" {
+  name = "trusted-permissions"
+  role = aws_iam_role.trusted_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow", 
+        Action = [
+          "s3:ListBucket",  
+          "s3:GetObject"    
+        ],
+        Resource = [
+          aws_s3_bucket.agro_data_lake.arn,       
+          "${aws_s3_bucket.agro_data_lake.arn}/*" 
+        ]
+      },
+      {
+        Effect = "Allow", Action = ["s3:PutObject"],
+        Resource = ["${aws_s3_bucket.agro_data_lake.arn}/trusted/*"] 
+      },
+      {
+        Effect = "Allow", Action = "lambda:InvokeFunction",
+        Resource = aws_lambda_function.agro_enricher.arn
+      },
+      {
+        Effect = "Allow", Action = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "enriched_permissions" {
+  name = "enriched-permissions"
+  role = aws_iam_role.enriched_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow", Action = ["s3:GetObject"],
+        Resource = ["${aws_s3_bucket.agro_data_lake.arn}/trusted/*"]
+      },
+      {
+        Effect = "Allow", Action = ["s3:PutObject"],
+        Resource = ["${aws_s3_bucket.agro_data_lake.arn}/enriched/*"]
+      },
+      {
+        Effect = "Allow", Action = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "scheduler_role" {
+  name = "agro-scheduler-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
       Action = "sts:AssumeRole"
       Effect = "Allow"
-      Principal = { 
-        Service = "lambda.amazonaws.com" 
-    }
+      Principal = { Service = "scheduler.amazonaws.com" }
     }]
   })
 }
 
-# Write Logs to CloudWatch 
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_policy" "s3_access" {
-  name        = "agro-s3-access"
-  description = "Allow Lambda to read/write to the Agro Data Lake"
-
+resource "aws_iam_role_policy" "scheduler_start_policy" {
+  name = "start-pipeline"
+  role = aws_iam_role.scheduler_role.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:ListBucket"
-      ]
       Effect   = "Allow"
-      Resource = [
-        "arn:aws:s3:::${aws_s3_bucket.agro_data_lake.id}",      
-        "arn:aws:s3:::${aws_s3_bucket.agro_data_lake.id}/*"     
-      ]
+      Action   = "lambda:InvokeFunction"
+      Resource = aws_lambda_function.agro_scraper.arn 
     }]
   })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_s3" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = aws_iam_policy.s3_access.arn
-}
-
-
-resource "aws_iam_role" "scheduler_role" {
-  name = "agro-scheduler-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "scheduler.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "scheduler_lambda_policy" {
-  name = "agro-scheduler-lambda-policy"
-  role = aws_iam_role.scheduler_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = "lambda:InvokeFunction"
-        Resource = aws_lambda_function.agro_scraper.arn
-      }
-    ]
-  })
-}
-
-
-resource "aws_iam_role" "glue_service_role" {
-  name = "agro-glue-service-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "glue.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "glue_service_role_policy_attachment" {
-  role       = aws_iam_role.glue_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
-}
-
-resource "aws_iam_policy" "glue_s3_access" {
-  name        = "agro-glue-s3-access"
-  description = "Allow Glue to read/write to the Data Lake bucket"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowS3ListAndRead"
-        Effect = "Allow"
-        Action = [
-          "s3:ListBucket",
-          "s3:GetObject",
-          "s3:PutObject"
-        ]
-        Resource = [
-          aws_s3_bucket.agro_data_lake.arn,       
-          "${aws_s3_bucket.agro_data_lake.arn}/*"
-        ]
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "glue_s3_attach" {
-  role       = aws_iam_role.glue_service_role.name
-  policy_arn = aws_iam_policy.glue_s3_access.arn
 }
